c059fb58dccf43d5d06f3ccba4f245e44ed14812
Tinker <dogun1228@naver.com>
2019-01-28 14:06:44 +0900

    Updates

---
+++ precache-manifest.be7b23ea22a64a2120144389f193d4b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "50b9f6d7acda110cdc1f",
+    "url": "/karin-home/static/css/main.471c8d8f.chunk.css"
+  },
+  {
+    "revision": "50b9f6d7acda110cdc1f",
+    "url": "/karin-home/static/js/main.50b9f6d7.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/karin-home/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "e23da5ea26ac4dbd550c",
+    "url": "/karin-home/static/js/runtime~main.e23da5ea.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/karin-home/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "272f9d93006d2c9c69a62a8b785a6b0a",
+    "url": "/karin-home/index.html"
+  }
+];
